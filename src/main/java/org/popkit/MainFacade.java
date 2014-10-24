c19335cbca8dc7b2d2f9088c9a86230575ddcdef
蒋国宝 <loveaborn@foxmail.com>
2014-10-24 11:42:55 +0800

    排序的例子

--- src/main/java/org/popkit/MainFacade.java
+++ src/main/java/org/popkit/MainFacade.java
@@ -4,6 +4,11 @@ package org.popkit;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * @author guobao.jiang
  * @date 10/3/14
@@ -22,6 +27,23 @@ public class MainFacade {
 
         }
 
+        List<Integer> intList = new ArrayList<Integer>();
+        intList.add(4);
+        intList.add(1);
+        intList.add(5);
+        intList.add(2);
+        intList.add(90);
+
+        System.out.println(intList.toString());
+
+        Collections.sort(intList, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1 - o2;      // 从小到大排序
+            }
+        });
+
+        System.out.println(intList.toString());
 
         String hel = "Hello";
         String say = "Hi," + hel + "world!";
