747c7903e29bdde0760ac51020eb1fc34f22ba98
蒋国宝 <loveaborn@foxmail.com>
2014-12-19 00:57:59 +0800

    ParseInt

--- src/main/java/org/popkit/motion/FTPMotion.java
+++ src/main/java/org/popkit/motion/FTPMotion.java
@@ -1,9 +1,21 @@
 package org.popkit.motion;
 
+import org.apache.commons.lang.StringUtils;
+
 /**
  * @author guobao.jiang
  * @date 12/8/14
  * @time 12:11 AM
  */
 public class FTPMotion extends BaseMotion{
+    public static void main(String[] args) {
+        String test = "-12";
+        Integer testInt = Integer.parseInt(test);
+        boolean isTrue = StringUtils.isNumeric(test);
+        if (isTrue) {
+            System.out.print(test + "is a Numeric" );
+        } else {
+            System.out.print(test + "is not a Numeric" + testInt);
+        }
+    }
 }
